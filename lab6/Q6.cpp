030af2b7c75289c2e336b84949499b0be7523271
KingBenny101 <benstin2727@gmail.com>
2021-06-16 08:53:56 +0530

    Update Q6.cpp

--- lab6/Q6.cpp
+++ lab6/Q6.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 
 int main(){
-    
+    char str[100];
+    char ch;
+    int i;
+    for(i = 0; (ch=cin.get())!=EOF;i++){
+        str[i]=ch;
+
+    }
+    str[i]='\0';
+
+    cout<<str;
     return 0;
 }
