13fc9077726ce164efcb993ce357166194c967e2
KingBenny101 <benstin2727@gmail.com>
2021-07-01 23:01:21 +0530

    Update q3.cpp

--- lab9/q3.cpp
+++ lab9/q3.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<string.h>
+#include <string.h>
 using namespace std;
 
 struct course
@@ -15,12 +15,60 @@ struct marks
     int mark;
 };
 
-struct student {
+struct students
+{
     int studentId;
     char studentName[20];
 };
 
 
-int main(){
+int findStudentName(course c[],marks m[],students s[], int cSize, int mSize, int sSize, char requiredCourse[]){
     
+}
+
+int main()
+{
+    int cSize, mSize, sSize;
+    cin >> cSize >> mSize >> sSize;
+
+    course *c;
+    marks *m;
+    students *s;
+
+    c = new course[cSize];
+    m = new marks[mSize];
+    s = new students[sSize];
+
+    for (int i = 0; i < cSize; i++)
+    {
+        cin >> c[i].courseId >> c[i].courseName;
+    }
+
+    for (int i = 0; i < mSize; i++)
+    {
+        cin >> m[i].studentId >> m[i].courseId >> m[i].mark;
+    }
+
+        for (int i = 0; i < cSize; i++)
+    {
+        cin >> c[i].courseId >> c[i].courseName;
+    }
+
+    for (int i = 0; i < sSize; i++)
+    {
+        cin >> s[i].studentId >> s[i].studentName;
+    }
+
+    char givenCourse[10];
+    cin >> givenCourse;
+
+    cout<< findStudentName(c,m,s,cSize,mSize,sSize,givenCourse)<<endl;
+
+    delete [] c;
+    delete [] m;
+    delete [] s;
+
+    return 0;
+
+
 }
