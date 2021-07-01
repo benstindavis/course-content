a0a5b19da5638663664c41697f8be815b46e4725
KingBenny101 <benstin2727@gmail.com>
2021-07-01 22:50:00 +0530

    Create q3.cpp

---
+++ lab9/q3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+struct course
+{
+    int courseId;
+    char courseName[10];
+};
+
+struct marks
+{
+    int studentId;
+    int courseId;
+    int mark;
+};
+
+struct student {
+    int studentId;
+    char studentName[20];
+};
+
+
+int main(){
+    
+}
