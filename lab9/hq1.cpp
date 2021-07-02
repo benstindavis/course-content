3e3ce6ac368e17200188d2614533f8504a4a3757
KingBenny101 <benstin2727@gmail.com>
2021-07-02 17:58:46 +0530

    Update hq1.cpp

--- lab9/hq1.cpp
+++ lab9/hq1.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 using namespace std;
-/*input
-4
-*/
 
 int main(){
-    int n;
-    cin>>n;
-    cout<<n*n;
-    return 0;
+    int x[5][5] = {
+        {1,2,3,4,5},
+        {6,7,8,9,10},
+        {11,12,13,14,15},
+        {16,17,18,19,20},
+        {21,22,23,24,25}
+    };
+    int n = 5;
+    
 }
